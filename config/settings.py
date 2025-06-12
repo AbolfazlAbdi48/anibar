3a2d6a124d543edd3862d02223900ad5ef663ad5
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2025-06-12 17:51:19 +0330

    static files config

--- config/settings.py
+++ config/settings.py
@@ -136,10 +136,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "assets")
 ]
 
-STATIC_ROOT = config('STATIC_ROOT')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = config('MEDIA_ROOT')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
