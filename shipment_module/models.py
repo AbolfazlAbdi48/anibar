3010716aa0365f7f9b56b4e1b05c1195894610eb
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2026-08-29 16:09:21 +0330

    Fix: Charge's amount field

--- shipment_module/models.py
+++ shipment_module/models.py
@@ -160,7 +160,7 @@ class Shipment(models.Model):
     manifest_no = models.CharField(max_length=255, null=True, blank=True, verbose_name="Manifest No")
 
     hscode = models.CharField(max_length=50, blank=True, null=True, verbose_name="HS Code")
-    extra_charges = models.DecimalField(max_digits=20, decimal_places=2, blank=True, null=True,
+    extra_charges = models.DecimalField(max_digits=50, decimal_places=2, blank=True, null=True,
                                         verbose_name="Extra Charges")
 
     operators = models.ManyToManyField(User, blank=True, related_name="operator_shipments", verbose_name="Operators")
@@ -318,7 +318,7 @@ class Console(models.Model):
 class Charge(models.Model):
     shipment = models.ForeignKey("Shipment", related_name="charges", on_delete=models.CASCADE)
     description = models.CharField(max_length=250)
-    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    amount = models.DecimalField(max_digits=50, decimal_places=2)
     currency = models.CharField(max_length=10, default="USD")
     payer = models.CharField(max_length=100, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
